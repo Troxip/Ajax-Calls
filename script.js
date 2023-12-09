516662a649753ff85698a44a49aa9eeb4badc6c7
Abdurahim <abdurahim.gaforov9547@gmail.com>
2023-12-09 22:15:06 +0100

    Learning to USE Ajex Calls (APIs)

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const currency = Object.values(data.currencies);
+    console.log(currency);
+    const language = Object.values(data.languages);
+    console.log(language);
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${data.flags.svg}">
+        <div class="country__data">
+          <h3 class="country__name">${data.name.official}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            data.population / 1000000
+          ).toFixed(1)} million<p>         
+          <p class="country__row"><span>🗣️</span>${
+            country === "tajikistan" ? language[1] : language[0]
+          }</p>
+          <p class="country__row"><span>💰</span>${currency[0].name}</p>
+        </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData("portugal");
+getCountryData("usa");
+getCountryData("palestine");
